033b3913c15e8597fcb85ee740f127129357b6fb
Charles <z0975201508@gmail.com>
2019-02-08 11:51:23 +0800

    排除CORS & 新增middle篩選Header和http方法 , 完成Backend資料於FrontEnd 呈現

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,21 @@ const express = require('express');
 
 // 設app為使用express
 const app = express();
+// Middleware 中介軟體
+app.use((req, res, next) => {
+  // 排除CORS
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // 允許Header內容
+  res.setHeader(
+    "Acess-Control-Allow-Header",
+    "Origin, X-Request-With, Content-Type, Accept"
+  );
+  // 允許方法
+  res.setHeader(
+    "Acess-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, PUT, OPTIONS")
+  next();
+})
 
 app.use('/api/posts', (req, res, next) => {
   const posts = [
