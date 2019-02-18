bd321832961376b198d26a5bc320dbbe2e627189
Charles <z0975201508@gmail.com>
2019-02-18 22:59:16 +0800

    重新綁定表單控制項 , 改採用reactiveForm

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { PostsService } from '../posts.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Post } from '../post.model';
@@ -14,6 +14,7 @@ export class PostCreateComponent implements OnInit {
   enteredContent = '';
   post: Post;
   isLoading = false;
+  form: FormGroup;
   private mode = 'create';
   private postId: string;
 
@@ -21,6 +22,16 @@ export class PostCreateComponent implements OnInit {
   constructor(public postsService: PostsService, public route: ActivatedRoute) {}
   // 初始化時執行
   ngOnInit() {
+    // 建立FormGroup
+    this.form = new FormGroup({
+      // 建立表單控制器
+      title: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(3)]
+      }),
+      // 建立表單控制器
+      content: new FormControl(null, {validators: [Validators.required]}),
+      image: new FormControl(null, {validators: [Validators.required]})
+    });
     // 訂閱動態路由的參數地圖(paramMap)
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       // 若裡面有postId
@@ -35,6 +46,10 @@ export class PostCreateComponent implements OnInit {
           this.isLoading = false;
           // 將post重新指定為該方法撈取的單筆資料 , 達成只編輯點選edit紐的資料效果
           this.post = {id: postData._id, title: postData.title, content: postData.content };
+          this.form.setValue({
+            title: this.post.title,
+            content: this.post.content
+          });
         });
       } else {
         this.mode = 'create';
@@ -42,21 +57,31 @@ export class PostCreateComponent implements OnInit {
       }
     });
   }
+  // 當選取上傳圖片
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    // 對單一控制器設定值 , 可傳入物件
+    this.form.patchValue({image: file});
+    // 選取控制器 , 並告訴Angular更新Value並馬上驗證
+    this.form.get('image').updateValueAndValidity();
+    // console.log(file);
+    // console.log(this.form);
+  }
 
-  onSavePost(form: NgForm) {
+  onSavePost() {
     // 若表單沒通過認證 return
-    if (form.invalid) {
+    if (this.form.invalid) {
       return;
     }
     this.isLoading = true;
     if (this.mode === 'create') {
       // 使用Service添加Post
-      this.postsService.addPost(form.value.title, form.value.content);
+      this.postsService.addPost(this.form.value.title, this.form.value.content);
     } else {
-      this.postsService.updatePost(this.postId, form.value.title, form.value.content);
+      this.postsService.updatePost(this.postId, this.form.value.title, this.form.value.content);
     }
 
     // 重製表單
-    form.resetForm();
+    this.form.reset();
   }
 }
