fe7060ae2590eb81e1846ca477bc75f7b5f94b51
Charles <z0975201508@gmail.com>
2019-03-05 21:57:37 +0800

    新增後端routes內user.js內signup的post方法處理 , 以bcrypt加密密碼

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,34 @@
 const express = require("express");
+// 引用bcrypt模組 , 用於加密密碼串
+const bcrypt = require("bcrypt");
+
+const User = require("../models/user");
 
 const router = express.Router();
 
 router.post("/signup", (req, res, next) => {
-
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+      const user = new User({
+        email: req.body.email,
+        password: hash
+      });
+      // 存入資料庫
+      user.save()
+        // 成功處理
+        .then(result => {
+          res.status(201).json({
+            message: "User Created!",
+            result: result
+          });
+        })
+        // 失敗處理
+        .catch(err => {
+          res.status(500).json({
+            error: err
+          });
+        });
+    });
 });
 
 module.exports = router;
